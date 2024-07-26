ead71a4ba6e04cefebd0e9ead4fd0f15b0d80671
pcguywilson <pcguywilson@gmail.com>
2024-07-26 16:06:36 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "name_prefix" {
+  default = "dev-tranque"
+}
+
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+variable "az_count" {
+  default = "2"
+}
+
+variable "healthcheck_path" {
+  default = "/"
+}
