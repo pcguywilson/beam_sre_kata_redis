06b7acdcb594616285b8801c0b50207e1679f427
pcguywilson <pcguywilson@gmail.com>
2024-07-25 20:07:46 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,193 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Define a tag map
+locals {
+  common_tags = {
+    Owner = "Swilson"
+    Name  = "beamkata"
+  }
+}
+
+# Use an existing VPC
+data "aws_vpc" "selected" {
+  id = var.vpc_id
+}
+
+data "aws_subnet_ids" "public" {
+  vpc_id = data.aws_vpc.selected.id
+  filter {
+    name   = "tag:Name"
+    values = ["public"]
+  }
+}
+
+data "aws_subnet_ids" "private" {
+  vpc_id = data.aws_vpc.selected.id
+  filter {
+    name   = "tag:Name"
+    values = ["private"]
+  }
+}
+
+data "aws_subnet" "public" {
+  count = length(data.aws_subnet_ids.public.ids)
+  id    = element(data.aws_subnet_ids.public.ids, count.index)
+}
+
+data "aws_subnet" "private" {
+  count = length(data.aws_subnet_ids.private.ids)
+  id    = element(data.aws_subnet_ids.private.ids, count.index)
+}
+
+# Create an ECS cluster
+resource "aws_ecs_cluster" "webapp_cluster" {
+  name = "beamkata-cluster"
+  tags = local.common_tags
+}
+
+# Create a security group
+resource "aws_security_group" "webapp_sg" {
+  name        = "beamkata-sg"
+  description = "Allow traffic to beamkata web app"
+  vpc_id      = data.aws_vpc.selected.id
+
+  ingress {
+    from_port   = 4567
+    to_port     = 4567
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
+}
+
+# Create an ElastiCache Redis cluster
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "beamkata-redis-cluster"
+  engine               = "redis"
+  node_type            = var.redis_node_type
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
+
+  security_group_ids = [aws_security_group.webapp_sg.id]
+  tags = local.common_tags
+}
+
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+  name       = "beamkata-redis-subnet-group"
+  subnet_ids = data.aws_subnet.private.*.id
+  tags       = local.common_tags
+}
+
+# Create an ECS task definition
+resource "aws_ecs_task_definition" "webapp_task" {
+  family                   = "beamkata-webapp-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name      = "beamkata-webapp"
+    image     = var.docker_image
+    essential = true
+    portMappings = [{
+      containerPort = 4567
+      hostPort      = 4567
+    }]
+    environment = [
+      {
+        name  = "REDIS_URL"
+        value = "redis://${aws_elasticache_cluster.redis.cache_nodes[0].address}:${aws_elasticache_cluster.redis.port}"
+      }
+    ]
+  }])
+
+  tags = local.common_tags
+}
+
+# Create an ECS service
+resource "aws_ecs_service" "webapp_service" {
+  name            = "beamkata-webapp-service"
+  cluster         = aws_ecs_cluster.webapp_cluster.id
+  task_definition = aws_ecs_task_definition.webapp_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = data.aws_subnet.public.*.id
+    security_groups = [aws_security_group.webapp_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.webapp_tg.arn
+    container_name   = "beamkata-webapp"
+    container_port   = 4567
+  }
+
+  tags = local.common_tags
+}
+
+# Create an Application Load Balancer
+resource "aws_lb" "webapp_lb" {
+  name               = "beamkata-webapp-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.webapp_sg.id]
+  subnets            = data.aws_subnet.public.*.id
+
+  enable_deletion_protection = false
+
+  tags = local.common_tags
+}
+
+# Create a target group for the load balancer
+resource "aws_lb_target_group" "webapp_tg" {
+  name     = "beamkata-webapp-tg"
+  port     = 4567
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.selected.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
+
+  tags = local.common_tags
+}
+
+# Create a listener for the load balancer
+resource "aws_lb_listener" "webapp_listener" {
+  load_balancer_arn = aws_lb.webapp_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.webapp_tg.arn
+  }
+
+  tags = local.common_tags
+}
