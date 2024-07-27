dce1bc4a183b6b43f28d89a664b954f228d7c3da
pcguywilson <pcguywilson@gmail.com>
2024-07-26 20:14:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = var.redis_node_type
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
+  parameter_group_name = "default.redis7"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
 
