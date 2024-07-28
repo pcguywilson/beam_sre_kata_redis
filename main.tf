2394490913b810c906bbb49248720d8727b18734
pcguywilson <pcguywilson@gmail.com>
2024-07-28 15:47:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -194,27 +194,6 @@ resource "aws_ecs_task_definition" "webapp_task" {
   tags = local.common_tags
 }
 
-resource "aws_ecs_service" "webapp_service" {
-  name            = "${var.app_name}-webapp-service"
-  cluster         = aws_ecs_cluster.webapp_cluster.id
-  task_definition = aws_ecs_task_definition.webapp_task.arn
-  launch_type     = "FARGATE"
-  desired_count   = 1
-
-  network_configuration {
-    subnets         = aws_subnet.public[*].id
-    security_groups = [aws_security_group.webapp_sg.id]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.webapp_tg.arn
-    container_name   = "${var.app_name}-webapp"
-    container_port   = 4567
-  }
-
-  tags = local.common_tags
-}
-
 resource "aws_lb" "webapp_lb" {
   name               = "${var.app_name}-webapp-lb"
   internal           = false
@@ -253,6 +232,31 @@ resource "aws_lb_listener" "webapp_http" {
   }
 
   tags = local.common_tags
+
+  depends_on = [aws_lb.webapp_lb]
+}
+
+resource "aws_ecs_service" "webapp_service" {
+  name            = "${var.app_name}-webapp-service"
+  cluster         = aws_ecs_cluster.webapp_cluster.id
+  task_definition = aws_ecs_task_definition.webapp_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = aws_subnet.public[*].id
+    security_groups = [aws_security_group.webapp_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.webapp_tg.arn
+    container_name   = "${var.app_name}-webapp"
+    container_port   = 4567
+  }
+
+  tags = local.common_tags
+
+  depends_on = [aws_lb_listener.webapp_http]
 }
 
 resource "aws_cloudwatch_log_group" "ecs_logs" {
