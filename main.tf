3354dd2391308e42f02f8812f309ab6de69bcd98
pcguywilson <pcguywilson@gmail.com>
2024-07-29 08:46:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -121,6 +121,35 @@ resource "aws_security_group" "webapp_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    security_groups = [aws_security_group.redis_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_security_group" "redis_sg" {
+  name        = "${var.app_name}-redis-sg"
+  description = "Security group for Redis cluster"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -140,7 +169,7 @@ resource "aws_elasticache_cluster" "redis" {
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
 
-  security_group_ids = [aws_security_group.webapp_sg.id]
+  security_group_ids = [aws_security_group.redis_sg.id]
   tags = local.common_tags
 }
 
